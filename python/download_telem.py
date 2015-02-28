b76159a82d1f8a6ce438d208507dc8ae56a53a7c
Andrew Pullin <pullin@berkeley.edu>
2015-02-27 17:58:51 -0800

    Fixing download_telem, now works with latest code.

--- python/download_telem.py
+++ python/download_telem.py
@@ -6,11 +6,15 @@ Contents of this file are copyright Andrew Pullin, 2013
 
 """
 from lib import command
-import time,sys
+import time,sys,os,traceback
 import serial
-import shared
 
-from or_helpers import *
+# Path to imageproc-settings repo must be added
+sys.path.append(os.path.dirname("../../imageproc-settings/"))
+sys.path.append(os.path.dirname("../imageproc-settings/"))  
+import shared_multi as shared
+
+from velociroach import *
 
 
 ###### Operation Flags ####
@@ -20,7 +24,7 @@ EXIT_WAIT   = False
 def main():    
     xb = setupSerial(shared.BS_COMPORT, shared.BS_BAUDRATE)
     
-    R1 = Robot('\x20\x52', xb)
+    R1 = Velociroach('\x20\x52', xb)
     
     shared.ROBOTS = [R1] #This is neccesary so callbackfunc can reference robots
     shared.xb = xb           #This is neccesary so callbackfunc can halt before exit
@@ -38,11 +42,19 @@ def main():
     numToDL = raw_input("How many samples to download? ")
     
     if numToDL > 0:
+        
+        #allocate an array to write the downloaded telemetry data into
         R1.numSamples = int(numToDL)
-        R1.imudata = [ [] ] * R1.numSamples
+        R1.telemtryData = [ [] ] * R1.numSamples
+        R1.clAnnounce()
+        print "Telemetry samples to save: ",R1.numSamples
+
         R1.runtime = 'UNKNOWN'
         R1.moveq = 'UNKNOWN'
 
+        blank_gait = GaitConfig()
+        R1.currentGait = blank_gait
+        
         # Pause and wait to start run, including leadin time
         print ""
         print "  ***************************"
@@ -53,30 +65,27 @@ def main():
 
         R1.downloadTelemetry(retry = False)
 
-    if EXIT_WAIT:  #Pause for a Ctrl + Cif specified
+    if EXIT_WAIT:  #Pause for a Ctrl + C , if desired
         while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
-                break
+            time.sleep(0.1)
 
     print "Done"
-    xb_safe_exit()
 
 
 #Provide a try-except over the whole main function
 # for clean exit. The Xbee module should have better
 # provisions for handling a clean exit, but it doesn't.
+#TODO: provide a more informative exit here; stack trace, exception type, etc
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         print "\nRecieved Ctrl+C, exiting."
-        shared.xb.halt()
-        shared.ser.close()
-    #except Exception as args:
-    #    print "\nGeneral exception:",args
-    #    print "Attemping to exit cleanly..."
-    #    shared.xb.halt()
-    #    shared.ser.close()
-    #    sys.exit()
+    except Exception as args:
+        print "\nGeneral exception from main:\n",args,'\n'
+        print "\n    ******    TRACEBACK    ******    "
+        traceback.print_exc()
+        print "    *****************************    \n"
+        print "Attempting to exit cleanly..."
+    finally:
+        xb_safe_exit(shared.xb)
